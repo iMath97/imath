33895fda4fd9f51c4f8a7e0db55e6b957c9e7372
iMath97 <camusmathias97@gmail.com>
2020-03-29 19:46:06 +0200

    header update

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(config){
 
     config.addPassthroughCopy("src/js");
     config.addPassthroughCopy("src/css");
+    config.addPassthroughCopy("src/img");
 
     return {
         passthroughFileCopy: true,
