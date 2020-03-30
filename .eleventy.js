cd47ced5ded5818b4dfdb4fb23267498948ee3da
iMath97 <camusmathias97@gmail.com>
2020-03-30 19:28:43 +0200

    update

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,6 @@ module.exports = function(config){
     config.addPassthroughCopy("src/js");
     config.addPassthroughCopy("src/css");
     config.addPassthroughCopy("src/img");
-    config.addPassthroughCopy("src/info");
 
     return {
         passthroughFileCopy: true,
